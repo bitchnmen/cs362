8b93e863b272301b8da20e07ea3117dec105b435
sheaffmc <mitchell.sheaffer@gmail.com>
2013-10-14 21:44:13 -0500

    creating a header file

---
+++ lab2/prime.h
@@ -0,0 +1,19 @@
+#ifndef MYSHELL_H
+#define MYSHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdbool.h>
+
+/* client.cc */
+
+/* server.cc */
+
+/* prime.cc */
+
+
+#endif
