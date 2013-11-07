00c70d94be4d844e3fa889d689ed3e917eea886f
Benjamin Singer <singerbj@uwec.edu>
2013-11-06 20:40:45 -0600

    added logic for mfqs

--- lab3/mfqs.cc
+++ lab3/mfqs.cc
@@ -4,15 +4,21 @@
 int mfqs(){
     int queues = -1;
     int q = -1;
+    int maxage = -1;
+
     cout << "\n\nSelected MFQS";
     cout << "\nEnter number of queues(1-5): ";
     cin >> queues;
     cout << "\n";
     
-    cout << "\nEnter size of the time quantum: ";
+    cout << "\nEnter size of the Time Quantum: ";
     cin >> q;
     cout << "\n";
     
+    cout << "\nEnter the Maximum Age Time: ";
+    cin >> q;
+    cout << "\n";
+
     if(queues < 1 && queues > 5){
         cout << "\nError: Invalid Number of Queues Input";
         exit(1);
@@ -22,7 +28,12 @@ int mfqs(){
         cout << "\nError: Invaild Time Quantum Input";
         exit(1);
     }
-	
+    
+    if(maxage < 1){
+        cout << "\nError: Invaild Maximum Age Input";
+        exit(1);
+	}
+
 	//getProcesses();
 	int numLines;
 	
@@ -31,6 +42,23 @@ int mfqs(){
 	cout << "numLines: " << numLines << endl;
 	
 	print_in_file(processes, &numLines);
-	
+
+    for (int i = 0; i < numlines; i++){
+        if(processes[i].get_burst > 0){
+
+            for(int j = 0; j < q; j++) { processes[i].set_burst(processes[i].get_burst() - 1) };
+            
+            if(processes[i].get_burst > 0){
+                if(processes[i].get_level < (queues * q)){
+                    processes[i].set_level(processes[i].get_level() + 1)
+                }else{
+                    if(processes.get_age()){
+                        processes[i].set_level(processes[i].get_level() - 1);
+                    }
+                } 
+            }
+        }
+    }
+    	
     
 }
