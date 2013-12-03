b1a744ddfdad76caf90db4446c0df04fa5b7e15d
Benjamin Singer <singerbj@uwec.edu>
2013-12-02 21:07:58 -0600

    stuff from stack

--- lab4/traffic.cc
+++ lab4/traffic.cc
@@ -1,34 +1,68 @@
 #include "traffic.h"
 
+pthread_mutex_t intersection_mutex;
 
 int main(int argc, char *argv[]){
-	street[4]; 
-	Q[4] ; //each queue for each street 
-	empty // intersection is empty (Boolean) 
- 
-	// car arrives at street[i] 
-	arrival(int i) { 
-		if (empty) { 
-			empty =false; 
-			drive(i); 
-		} else { 
-			Q[i]++; 
-			// wait for someone to signal that it is street[i]’s 
-			turn to go 
-			drive[i]; 
-		} 
-	} //arrival(i)
+    int NUMCARS = -1;
+    cout << "\nSelect number of cars: ";
+    cin >> NUMCARS;
+    
+    if(NUMCARS < 1){
+        cout << "\n\n Please select a valid amount of cars. \n\n";
+        exit(1);
+    }
+
+    int index, tid;
+    int carids[NUMCARS];
+    pthread_t carthreads[NUMCARS];
+    
+    /* Start up a thread for each car*/ 
+    for(index = 0; index <NUMCARS; index++){
+        carids[index] = index;
+        tid = pthread_create(&carthreads[index], NULL, approachintersection,  (void*)&carids[index]);
+    }
+
+    /* Wait for every car thread to finish */
+    for(index = 0; index <NUMCARS; index++){
+        pthread_join(carthreads[index], NULL);
+    }
+    printf("Done\n");
+    return 1;
+}
+static void * approachintersection(void* arg){
+	unsigned int * carnumberptr;
+	unsigned int carnumber;
+	int cardirection = rndom();
 	
-	// departure of car from intersection 
-	departure (int i) { 
-		// one car departs
-		// pick the next intersection for next car to go 
-		// if no cars are in queues, empty = true 
-		
-	}
+    carnumberptr = (unsigned int*) arg;
+	carnumber = (unsigned int) *carnumberptr;
+
+	gostraight(cardirection, carnumber);
+
+	return (void*)carnumberptr;
+}
+
+static void gostraight(int cardirection, unsigned int carnumber){
+    pthread_mutex_lock(&intersection_mutex);
+    switch(cardirection){
+        case 1:
+            printf("Car %d, Moving North-South\n", carnumber);
+            break;
+        case 2:
+            printf("Car %d, Moving South-North\n", carnumber);
+            break;
+        case 3:
+            printf("Car %d, Moving East-West\n", carnumber);
+            break;
+        case 4:
+            printf("Car %d, Moving West-East\n", carnumber);
+            break;
+        }
+    pthread_mutex_unlock(&intersection_mutex);
+}
 
 /* Returns a random number from 0 to 1 */ 
-double rndom() { 
+int rndom() { 
     const long A = 48271; 
     const long M = 2147483647; 
     const long Q = M/A; 
@@ -40,6 +74,20 @@ double rndom() {
         state = t;
     } else { 
         state = t + M;
-        return ((double) state/M); 
+        double temp = ((double) state/M); 
+        
+        int r;
+
+        if(temp >= .75){
+            r = 1;
+        }else if(temp >= .50){
+            r = 2;
+        }else if(temp >= .25){
+            r = 3;
+        }else{
+            r = 4;
+        }
+
+        return r;
     }
 }
--- lab4/traffic.h
+++ lab4/traffic.h
@@ -1,18 +1,28 @@
 #define PRIME_H
 
-#include <errno.h>
 #include <iostream>
-#include <signal.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <cmath>
 #include <pthread.h>
-#include <netdb.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <math.h>
+#include <map>
+#include <queue>
+#include <time.h>
+
+using namespace std;
+
+
+static void * approachintersection(void* arg);
+
+static void gostraight(int cardirection, unsigned int carnumber);
+
+int rndom();
 
 
